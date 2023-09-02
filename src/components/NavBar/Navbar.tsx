52a761db918aca6e48f07a45f5f7102b6185141c
lazargas <82470472+lazargas@users.noreply.github.com>
2023-09-02 17:40:57 +0530

    Final Project complete

--- src/components/NavBar/Navbar.tsx
+++ src/components/NavBar/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <>
     <div className={style.navbar} >
      <div className='text-white hover:text-[25px] ease-in-out transition-all cursor-none ' >VOTING DAO</div>
-     <a className='text-white hover:text-[#0ff] ease-in-out transition-all cursor-none ' href='https://safevoters.netlify.app' >LAUNCH {`->`} </a>
+     <a className='text-white hover:text-[#0ff] ease-in-out transition-all cursor-none ' href='https://voting-dao-lazargas.vercel.app/' >LAUNCH {`->`} </a>
     </div>
     
     </>
